23612ddeeb637fca36695cb5256c05d9cab0a3fc
RyuSoul <quentin.barriere@gmail.com>
2025-10-03 15:52:49 +0200

    adding HashRouter

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,13 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
